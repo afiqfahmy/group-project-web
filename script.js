fb27a15f1b9ea7834b84b1d61fb66c1222cc2853
Simon8282005 <simonjava2019@gmail.com>
2026-01-08 21:57:56 +0800

    Edit somethings

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 main();
 
+// Globbal dictionary use by js for cart function
+// Unique id: 
+// ip17pm   : 001
+// ipadair  : 002
+// mac air  : 003
+var cart = [];
+
 function main() {
     fade_on_scroll();
     fluid_scrolling_effect();
+
+    // Initialize the cart
+    cart = [];
 }
 
 // Page transition (fade out on link click)
@@ -25,7 +35,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 window.location.href = link.href;
             }, 250);
         });
-
     });
 });
 
@@ -45,6 +54,7 @@ function fade_on_scroll() {
     document.querySelectorAll('.fade-on-scroll')
         .forEach(el => observer.observe(el));
 }
+
 
 function fluid_scrolling_effect() {
     // Lazy to import the outside js file for smooth for every single file
@@ -66,6 +76,29 @@ function fluid_scrolling_effect() {
     }
     // Append script to documnet head
     document.head.appendChild(script);
-    
 }
 
+// Testing the cart function in js
+function add_to_cart(product_id, product_name, quantity) {
+    // First check the id of the product, if the product got same id then update the quantity
+    // if not add new product
+    console.log("Item has been added !");
+    //if (cart.lenght == 0) {
+        cart.push("Product ID: " + product_id + "Product Name: " + product_name + "Quantity: " + quantity);
+    // if the id are the same
+        for (var i = 0; i < cart.length; i++) {
+            console.log(cart[i]);
+        }
+        
+    }
+
+
+// Function to reduce the quantity of the product
+function delete_from_cart_quantity(product_id, product_name, quantity) {
+
+}
+
+// Function delete all same type of product from the cart
+function delete_from_cart() {
+
+}
